d9d527615ea6b6bf461d9824c0d485d9558b3447
Dillon Chanis <dillonchanis92@gmail.com>
2018-01-13 16:28:36 -0500

    Removing User registration handling to Auth controller.

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,42 +1,8 @@
 'use strict'
 
 const User = use('App/Models/User')
-const { validate } = use('Validator')
 
 class UserController {
-  async store ({ request }) {
-    /**
-     * Getting needed paramaters.
-     */
-    const data = request.only(['username', 'email', 'password'])
-
-    /**
-     * Validating our data.
-     */
-    const validation = await validate(data, {
-      username: 'required|unique:users',
-      email: 'required|email|unique:users',
-      password: 'required'
-    })
-
-    /**
-     * If validation fails, early return with validation messages
-     */
-    if (validation.fails()) {
-      return validation.messages()
-    }
-
-    /**
-     * Create user in the database.
-     */
-    const user = await User.create(data)
-
-    /**
-     * Return newly created user
-     */
-    return user
-  }
-
   /**
    * Get a user.
    */
@@ -51,7 +17,7 @@ class UserController {
     try {
       user = await User.find(id)
     } catch (err) {
-      return err
+      return err // todo
     }
 
     return user
