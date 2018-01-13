e7dc06c15ba85fdcc556c6bfdf26ab6d0061e9ee
Dillon Chanis <dillonchanis92@gmail.com>
2018-01-13 16:28:05 -0500

    Refactoring controller to be responsible for all aspects of auth lifecycle.

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -1,24 +1,67 @@
 'use strict'
 
 const User = use('App/Models/User')
+const { validate } = use('Validator')
 
 class AuthController {
-  // Create JWT
-  async create ({ auth, request, response, session }) {
-    const { email, password } = request.all()
-    const user = await User.findBy('email', email)
+  async generateToken (auth, email, password) {
     let token
 
     try {
       token = await auth.attempt(email, password)
     } catch (e) {
-      return e
+      return e // todo
     }
 
-    return {
-      token,
-      user
+    return token
+  }
+
+  async login ({ auth, request, response }) {
+    const { email, password } = request.all()
+    const user = await User.findBy('email', email)
+    const token = await this.generateToken(auth, email, password)
+
+    return response.status(200).json({ token, user })
+  }
+
+  async register ({ auth, request, response }) {
+    /**
+     * Getting needed paramaters.
+     */
+    const data = request.only(['username', 'email', 'password'])
+
+    /**
+     * Validating our data.
+     */
+    const validation = await validate(data, {
+      username: 'required|unique:users',
+      email: 'required|email|unique:users',
+      password: 'required'
+    })
+
+    /**
+     * If validation fails, early return with validation messages
+     */
+    if (validation.fails()) {
+      return validation.messages()
     }
+
+    /**
+     * Create user in the database.
+     */
+    const user = await User.create(data)
+    const token = await this.generateToken(auth, data.email, data.password)
+
+    /**
+     * Return newly created user
+     */
+    return response.status(200).json({ token, user })
+  }
+
+  async logout ({ request, response }) {
+    await request.auth.logout()
+
+    return response.redirect('/')
   }
 }
 
