36ad313c56116d9cb0217db3d5623c5541bfa04f
Dillon Chanis <dillonchanis92@gmail.com>
2018-01-15 20:27:53 -0500

    Refactoring actions to use async/await

--- resources/assets/js/app/Auth/vuex/actions.js
+++ resources/assets/js/app/Auth/vuex/actions.js
@@ -1,67 +1,37 @@
-import { isEmpty } from 'lodash'
-import { setHttpToken } from '../../../helpers'
-import localforage from 'localforage'
-
 const baseUrl = '/api/v1'
 
-export const register = ({ dispatch }, { payload, context }) => {
-  return axios.post(`${baseUrl}/register`, payload).then((response) => {
-    dispatch('setToken', response.data.token).then(() => {
-      dispatch('fetchUser', response.data.user.id)
-    })
-  }).catch((error) => {
-    console.log(error)
-  })
-}
-
-export const login = ({ dispatch }, { payload, context }) => {
-  return axios.post(`${baseUrl}/login`, payload).then((response) => {
-    dispatch('setToken', response.data.token).then(() => {
-      dispatch('fetchUser', response.data.user.id)
-    })
-  }).catch((error) => {
-    console.log(error)
-  })
-}
-
-export const logout = ({ dispatch }) => {
-  return axios.post(`${baseUrl}/logout`).then((response) => {
-    dispatch('clearAuth')
-  })
-}
-
-export const fetchUser = ({ commit }, id) => {
-  return axios.get(`${baseUrl}/user/${id}`).then((response) => {
+export const register = async ({ commit }, { payload, context }) => {
+  try {
+    const user = await axios.post(`${baseUrl}/register`, payload)
     commit('setAuthenticated', true)
-    commit('setUserData', response.data)
-  })
-}
-
-export const setToken = ({ commit, dispatch }, token) => {
-  if (isEmpty(token)) {
-    return dispatch('checkTokenExists').then((token) => {
-      setHttpToken(token)
-    })
+    commit('setUserData', user)
+  } catch (error) {
+    context.errors = error.response.data[0]
   }
-
-  commit('setToken', token)
-  setHttpToken(token)
 }
 
-export const checkTokenExists = ({ commit, dispatch }) => {
-  return localforage.getItem('authtoken').then((token) => {
-    if (isEmpty(token)) {
-      return Promise.reject('NO_STORAGE_TOKEN')
-    }
+export const login = async ({ commit }, { payload, context }) => {
+  try {
+    const user = await axios.post(`${baseUrl}/login`, payload)
+    commit('setAuthenticated', true)
+    commit('setUserData', user)
+  } catch (error) {
+    context.errors = error.response.data[0]
+  }
+}
+export const logout = async ({ dispatch }) => {
+  await axios.post(`${baseUrl}/logout`)
+  dispatch('clearAuth')
+}
 
-    return Promise.resolve(token)
-  })
+export const fetchUser = async ({ commit }, id) => {
+  const user = await axios.get(`${baseUrl}/user/${id}`)
+  commit('setAuthenticated', true)
+  commit('setUserData', user)
 }
 
 export const clearAuth = ({ commit }) => {
   commit('setAuthenticated', false)
   commit('setUserData', null)
-  commit('setToken', null)
   commit('clearIntended')
-  setHttpToken(null)
 }
