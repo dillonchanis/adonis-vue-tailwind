daf0970e87091ce6281a1122541c9e42e1060287
Dillon Chanis <dillonchanis92@gmail.com>
2018-01-16 20:33:50 -0500

    Setting up eslint.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,10 +1,25 @@
-const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss');
+const mix = require('laravel-mix')
+const tailwindcss = require('tailwindcss')
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .options({
-     processCssUrls: false,
-     postCss: [ tailwindcss('./tailwind.js') ],
-   })
-   .setPublicPath('public');
+  .sass('resources/assets/sass/app.scss', 'public/css')
+  .options({
+    processCssUrls: false,
+    postCss: [ tailwindcss('./tailwind.js') ]
+  })
+  .webpackConfig({
+    module: {
+      rules: [
+        {
+          test: /.(js|vue)$/,
+          loader: 'eslint-loader',
+          enforce: 'pre',
+          exclude: /node_modules/,
+          options: {
+            formatter: require('eslint-friendly-formatter')
+          }
+        }
+      ]
+    }
+  })
+  .setPublicPath('public')
