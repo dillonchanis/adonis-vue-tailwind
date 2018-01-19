014f2302ec8ea9491e8d86ddb4dbd51ee1b355e4
Dillon Chanis <dillonchanis92@gmail.com>
2018-01-18 21:33:42 -0500

    Creating Base component for authenticated dashboard routes.

---
+++ resources/assets/js/Base.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <AppSidebar />
+    <div class="wrapper"
+         :class="{ 'md:ml-80': open }">
+      <AppNavbar />
+      <div class="w-full max-w-lg">
+        <div class="md:ml-4">
+          <router-view />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import { AppSidebar, AppNavbar } from '@layout/'
+
+export default {
+  name: 'Base',
+  components: {
+    AppSidebar,
+    AppNavbar
+  },
+  computed: {
+    ...mapGetters({
+      open: 'sidebar/open'
+    })
+  }
+}
+</script>
+
+<style scoped>
+.wrapper {
+  transition: .3s cubic-bezier(.4,0,.2,1);
+  will-change: margin;
+}
+</style>
--- resources/assets/js/app/routes.js
+++ resources/assets/js/app/routes.js
@@ -1,4 +1,23 @@
+import Base from '@/Base'
+import PageNotFound from '@/PageNotFound'
 import auth from './Auth/routes'
 import dashboard from './Dashboard/routes'
 
-export default [...auth, ...dashboard]
+export default [
+  ...auth,
+  {
+    path: '/',
+    component: Base,
+    meta: {
+      guest: false,
+      needsAuth: true
+    },
+    children: [
+      ...dashboard
+    ]
+  },
+  {
+    path: '*',
+    component: PageNotFound
+  }
+]
